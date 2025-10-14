fa09aaa3e5cd658f89d71b76f8773e30573fda4e
Samuel Monroe <spat.monroe@gmail.com>
2025-10-14 11:21:46 +0900

    Generate GitHub page documentation (#21)
    * Initial Django Github workflow
    
    * Fix trigger conditions
    
    * Remove unneeded jobs
    
    * Set default working directory
    
    * Use UV
    
    * Fix directory positioning
    
    * Fix jobs by giving uv context
    
    * Fix mypy
    
    * Add a release Github Workflow
    
    * Align releases versions
    
    * Add GitHub workflow for building and publishing documentation
    
    * Remove type declarations from Storybook build (not needed)
    
    * Fix command
    
    * Update docs workflow to use custom actions for Node.js and Python setup
    
    * Joint documentation publication
    
    * Ruff format
    
    * Fix Mypy issue
    
    * Only works on default main branch
    
    * Target proper Pypi url project for release workflow

--- django/nside_wefa/authentication/utils/settings_initialization.py
+++ django/nside_wefa/authentication/utils/settings_initialization.py
@@ -1,3 +1,11 @@
+"""
+Initialize underlying authentication framework defaults based on configured authentication types.
+
+This module inspects the project's NSIDE_WEFA authentication settings at app
+startup and configures REST framework's default authentication and permission
+classes accordingly.
+"""
+
 from django.conf import settings
 
 from nside_wefa.authentication.constants import AUTH_TYPE_TOKEN, AUTH_TYPE_JWT
@@ -9,6 +17,12 @@ logger = logging.getLogger(__name__)
 
 
 def initialize_settings():
+    """Apply DRF defaults derived from ``NSIDE_WEFA.AUTHENTICATION.TYPES``.
+
+    Depending on whether ``TOKEN`` and/or ``JWT`` authentication are enabled,
+    this sets ``REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES']`` and ensures
+    ``IsAuthenticated`` is used as the default permission class.
+    """
     authentication_types = get_authentication_types()
     default_authentication_classes = []
     default_permission_classes = [
